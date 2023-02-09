5d2336feba5bba331b23926c72ba5a3e47588cbb
Tornyai Laurentiu Andrei <121413180+thedrew94@users.noreply.github.com>
2023-02-09 17:26:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const containers = document.querySelectorAll('.bck-color');
 const colors = ['rgba(255, 0, 0, 0.1)', 'rgb(198, 148, 218, 0.1)', 'rgba(0, 0, 255, 0.1)', 'rgb(148, 218, 169, 0.1)', 'rgba(255, 192, 203, 0.1)', 'rgba(255, 255, 0, 0.1)'];
 
-alert('This project is a replica of the official Sky Tree Tokyo website.\nIt was created with the goal of perfecting my skills in JavaScript, HTML, and CSS.\nSome features are still under development. Some features may differ from the the original.\nThis project was completed by Tornyai Laurentiu Andrei.');
+alert("This project is a replica of the official Sky Tree Tokyo website.\n\
+It was created with the goal of refining my skills in JavaScript, HTML, and CSS.\n\
+Some features are still under development and may differ from the original.\n\
+This project was completed by Tornyai Laurentiu Andrei.");
 
 containers.forEach((container, index) => {
   container.addEventListener('mouseenter', function() {
